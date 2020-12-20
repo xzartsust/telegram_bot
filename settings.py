c8ed277c9c71fa1ba65ce7753ff86f689f65929b
xzartsust <igru1234567@gmail.com>
2020-12-20 19:47:54 +0200

    up

---
+++ settings.py
@@ -0,0 +1,7 @@
+import dj_database_url
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn= psycopg2.connect(DATABASE_URL, sslmode='require')
+DATABASE_URL['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
